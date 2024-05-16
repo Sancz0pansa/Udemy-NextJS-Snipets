1c28a7a31a9fa51ef4bd33dc1bd281cbfd101356
Hubert Wiktor <hubertwiktor6@gmail.com>
2024-05-16 18:44:18 +0200

    Create Snippet form created

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,6 +1,21 @@
 
 export default function SnippetCreatePage() {
   return (
-    <div>New Snippet</div>
+    <form action="">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">Title</label>
+          <input id="title" type="text" name="title" className="border rounded p-2 w-full" />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">Code</label>
+          <textarea id="code" name="code" className="border rounded p-2 w-full" />
+        </div>
+        
+        <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
+      </div>
+    </form>
   );
 }
